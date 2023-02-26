ea376f678b402cc9effe30f4da03766be8464e8b
milo18818 <vippugster@gmail.com>
2023-02-26 13:49:53 +0000

    bug fixes and added pc score and winner message at end game

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
 RCPscore = 0
+pcscore = 0
 for x in range(3):
     player = input("pick: rock (R), paper (P), scisores (S)")
+    player = player.lower()
     print(player,"vs", end=" ")
     chosen = randint(1,3)
     computer = 0
@@ -19,18 +21,29 @@ for x in range(3):
         print("draw")
     elif(player == "r" and computer == "s"):
         print("player wins")
-        RCPscore =+ 1
+        RCPscore += 1
     elif(player == "r" and computer == "p"):
         print("computer wins")
+        pcscore += 1
     elif(player == "p" and computer == "r"):
         print("player wins")
-        RCPscore =+ 1
+        RCPscore += 1
     elif(player == "p" and computer == "s"):
         print("computer wins")
+        pcscore += + 1
     elif(player == "s" and computer == "p"):
         print("player wins")
-        RCPscore =+ 1
+        RCPscore += 1
+    elif(player == "s" and computer == "r"):
+        print("computer wins")
+        pcscore += 1
     else:
         (player == "p" and computer == "s")
         print("computer wins")
-print("your score is " + str(RCPscore))
+print("your score is " + str(RCPscore) + " the computers score is " + str(pcscore))
+if RCPscore > pcscore:
+    print("player wins")
+elif RCPscore < pcscore:
+    print("computer wins")
+else:
+    print("draw")
