b0ca39bf2ed872090cd1d477e6034186e538d369
milo18818 <vippugster@gmail.com>
2023-03-02 20:24:35 +0000

    added special options and round editer

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from random import randint
 RCPscore = 0
 pcscore = 0
-for x in range(3):
+
+print("select gamemode")
+gametype = input()
+if gametype == ("s"):
+    print("special powers enabled")
+print("best of ___")
+round = input()
+for x in range (int(round)):
     player = input("pick: rock (R), paper (P), scisores (S)")
     player = player.lower()
     print(player,"vs", end=" ")
     chosen = randint(1,3)
     computer = 0
-
     if (chosen == 1):
         computer = "r"
     elif(chosen == 2):
         computer = "p"
     else:
-        (chosen == 3)
-        computer = "s"
+        if(chosen == 3):
+            computer = "s"
     print (computer)
 
     if player == computer:
@@ -41,12 +47,29 @@ for x in range(3):
         print("player wins")
         RCPscore += 1
     else:
-        (player == "p" and computer == "s")
-        print("computer wins")
+        if player == "p" and computer == "s":
+          print("computer wins")
+
+    if gametype == ("s"):
+        if player == "d" and computer == "r":
+            print("test")
+            pcscore -= 1
+        if player == "d" and computer == "p":
+            print("test")
+            pcscore -= 1
+        if player == "d" and computer == "s":
+            print("test")
+            pcscore -= 1
+        if player == "b" and computer == "r":
+            print("blocked")
+        if player == "b" and computer == "p":
+            print("blocked")
+        if player == "b" and computer == "s":
+            print("blocked")
 print("your score is " + str(RCPscore) + " the computers score is " + str(pcscore))
 if RCPscore > pcscore:
-    print("player wins")
+    print("!PLAYER WINS!")
 elif RCPscore < pcscore:
-    print("computer wins")
+    print("!COMPUTER WINS!")
 else:
-    print("draw")
+    print("!DRAW!")
